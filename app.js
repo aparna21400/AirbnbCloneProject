93d66d625da9593183db7461fceef9479662f9e9
Aparna <aparnamishra2003@gmail.com>
2025-10-28 00:16:41 +0530

    update some changes

--- app.js
+++ app.js
@@ -36,8 +36,10 @@ async function main() {
     });
 }
 
-app.set("views", path.join(__dirname, "/views"));
+process.chdir(__dirname);
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
